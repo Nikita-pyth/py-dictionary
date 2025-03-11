543b153874d665400876ec69662fd07285ece59f
Nikita-pyth <siorito321@gmail.com>
2025-03-11 11:52:20 +0200

    Small adjustments

--- app/custom_dict.py
+++ app/custom_dict.py
@@ -1,12 +1,15 @@
-from typing import Hashable, Any, Iterator
+from typing import Any, Hashable
 
-from app.node import Node
+from node import Node
 
 
 class CustomDict:
+    _no_value = object()
+    _deleted_marker = object()
+
     def __init__(self) -> None:
         self.capacity: int = 8
-        self.dictionary: list[Node | None] = [None] * self.capacity
+        self.dictionary: list[Node | object | None] = [None] * self.capacity
         self.load_factor: float = 2 / 3
         self._used_cells_counter: int = 0
 
@@ -20,19 +23,18 @@ class CustomDict:
     def __setitem__(self, key: Hashable, value: Any) -> None:
         new_node = Node(key, value)
         cell_to_occupy = hash(key) % self.capacity
-        if not self.dictionary[cell_to_occupy]:
-            self.dictionary[cell_to_occupy] = new_node
-            self._used_cells_counter += 1
-            return
-        elif self.dictionary[cell_to_occupy].key == key:
-            self.dictionary[cell_to_occupy].value = value
-        else:
-            for i in range(self.capacity):
-                index = (cell_to_occupy + i) % self.capacity
-                if self.dictionary[index] is None:
-                    self.dictionary[index] = new_node
-                    self._used_cells_counter += 1
-                    break
+        for i in range(self.capacity):
+            index = (cell_to_occupy + i) % self.capacity
+            if (
+                self.dictionary[index] is None
+                or self.dictionary[index] is self._deleted_marker
+            ):
+                self.dictionary[index] = new_node
+                self._used_cells_counter += 1
+                break
+            elif self.dictionary[index].key == key:
+                self.dictionary[index].value = value
+                break
 
         if self._used_cells_counter >= self.threshold:
             self._resize()
@@ -41,60 +43,56 @@ class CustomDict:
         index = hash(key) % self.capacity
         for i in range(self.capacity):
             index = (index + i) % self.capacity
-            if self.dictionary[index] and self.dictionary[index].key == key:
+            if self.dictionary[index] is None:
+                break
+            if (
+                self.dictionary[index] is not self._deleted_marker
+                and self.dictionary[index].key == key
+            ):
                 return self.dictionary[index].value
         raise KeyError(f"Key {key} is not found in the dictionary")
-
-    def clear(self) -> None:
-        self.__init__()
 
     def __delitem__(self, key: Hashable) -> None:
         index = hash(key) % self.capacity
         for i in range(self.capacity):
             index = (index + i) % self.capacity
-            if self.dictionary[index] and self.dictionary[index].key == key:
-                self.dictionary[index] = None
+            if self.dictionary[index] is None:
+                break
+            if (
+                self.dictionary[index] is not self._deleted_marker
+                and self.dictionary[index].key == key
+            ):
+                self.dictionary[index] = self._deleted_marker
                 self._used_cells_counter -= 1
                 return
-        raise KeyError(f"Key {key} not found in the dictionary."
+        raise KeyError(f"Key {key} is not present in the dictionary."
                        f" Unable to delete.")
 
-    def get(self, key: Hashable, default: Any = None) -> Any:
-        try:
-            return self.__getitem__(key)
-        except KeyError:
-            return default
-
-    def pop(self, key: Hashable, default: Any = None) -> Any:
+    def pop(self, key: Hashable, default: Any = _no_value) -> Any:
         index = hash(key) % self.capacity
         for i in range(self.capacity):
             index = (index + i) % self.capacity
-            if self.dictionary[index] and self.dictionary[index].key == key:
+            if self.dictionary[index] is None:
+                break
+            if (
+                self.dictionary[index] is not self._deleted_marker
+                and self.dictionary[index].key == key
+            ):
                 temp = self.dictionary[index].value
-                self.dictionary[index] = None
+                self.dictionary[index] = self._deleted_marker
                 self._used_cells_counter -= 1
                 return temp
-        if default is None:
-            raise KeyError(f"Key {key} not found in the dictionary."
+        if default is self._no_value:
+            raise KeyError(f"Key {key} is not present in the dictionary."
                            f" Unable to pop.")
 
         return default
-
-    def update(self, **kwargs) -> None:
-        for key, value in kwargs.items():
-            self.__setitem__(key, value)
 
     def _resize(self) -> None:
         old_dict = self.dictionary
         self.capacity *= 2
         self.dictionary = [None] * self.capacity
         self._used_cells_counter = 0
-
         for node in old_dict:
-            if node is not None:
-                self[node.key] = node.value
-
-    def __iter__(self) -> Iterator:
-        for node in self.dictionary:
-            if node is not None:
-                yield node.key
+            if isinstance(node, Node):
+                self.__setitem__(node.key, node.value)
